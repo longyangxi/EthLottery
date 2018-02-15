6bddb85df3277463f3547853688699b74b07e948
longames0 <longames@qq.com>
2018-02-15 14:09:31 +0800

    改进

--- EthLottery.sol
+++ EthLottery.sol
@@ -3,7 +3,7 @@ contract Owner{
   //拥有者
   address public owner;
   //构造器
-  function Owner() {
+  function Owner() public {
       owner = msg.sender;
   }
   //函数修饰符 限定创建者调用
@@ -19,75 +19,64 @@ contract Owner{
   }
 }
 //投资合同
-contract EthLottery is Owner{
-
+contract EwaLottery is Owner{
+  bool public running = true;                                       //合同是否在运行 
   address[] public bettors;                                          //所有下注者地址
 
   address[] public luckyBettors;                                     //幸运奖者
-  address[] public bigLuckyBettors;                                  //幸运大奖者
 
-  uint32 public minPlayers = 10;                                     //开奖基数，凑够多少人开奖
-  uint[] public lotteryCountHistory;                                 //开大奖中间开过的小奖励次数
-  
-  uint public baseBet = 10 finney;                                   //最少投注金额
-  uint public poolExtract = 10 finney;                               //每次开奖奖池抽取
-  uint public bigLuckyValue = 200 finney;                             //大奖奖励数量
-  uint public bigLuckyPlayers = 5;                                   //大奖奖励玩家数量
-  
-  
+  uint32 public minPlayers = 6;                                     //开奖基数，凑够多少人开奖
+  uint public luckyFee = 1 finney;                                   //开小奖返还矿工费
+  uint public baseBet =  5 finney;                                   //最少投注金额
+
   uint private _seed = 0;                                            //随机种子
-
-  modifier limitAmount(uint value){
+    
+  modifier limitValue(uint value){
     require(value >= baseBet);
     _;
   }
-
+  modifier limitPause(){
+    require(running);
+    _;
+  }
   //投注
-  function Bet() limitAmount(msg.value) payable external {
+  function Bet() limitValue(msg.value) payable limitPause external {
+    //数组dai'lai带来 0.0008ETH的gas 
     bettors.push(msg.sender);
-    
-    //检查小奖
-    if(!CheckLucky()) {
-        //小奖没有则检查大奖
-        CheckBigLucky();
+    //如果发送的超过基本投注记录余额
+    uint remain = msg.value - baseBet;
+    if(remain > 0) {
+        // playerEthers[msg.sender] += remain;
+        //多余的ehter直接返还 
+        msg.sender.transfer(remain);
     }
+    //检查小奖
+    CheckLucky();
   }
-
-  //设置每人投资金额
+  
+  //设置合同暂停状态
+  function SetPause(bool flag) onlyOwner external {
+    running = flag;
+  }
+  //设置每人投资金额，单位分
   function SetMinBet(uint newBet) onlyOwner external {
-      baseBet = newBet;
-  }
-  //设置每次开奖金额
-  function SetPoolExtract(uint extract) onlyOwner external {
-      poolExtract = extract;
-  }
-  //设置大奖金额
-  function SetBigLuckyValue(uint bigValue) onlyOwner external {
-      bigLuckyValue = bigValue;
-  }
-    //设置大奖人数
-  function SetBigLuckyPlayers(uint players) onlyOwner external {
-      bigLuckyPlayers = players;
+      uint bet = newBet * 1 finney;
+      baseBet = bet;
   }
   //设置抽奖人数基数
   function SetMinPlayers(uint32 min) onlyOwner external {
+      require(min >= 2);
       minPlayers = min;
   }
-
     //小奖大小
   function GetLuckyValue() public view returns(uint) {
-      //奖金为玩家数量 * 基础下注 - 奖池抽取
-      return minPlayers * baseBet - poolExtract;
+      //奖金为玩家数量 * 基础下注 - 矿工费
+      return minPlayers * baseBet - luckyFee;
   }
 
   //查看人数是否够开小奖
-  function IsLuckReady() public view returns (bool){
+  function IsLuckReady() private view returns (bool){
     return bettors.length >= (luckyBettors.length + 1) * minPlayers;
-  }
-
-  //是否可开大奖,考虑40+9的情况，最后必须保证余额要多于最后9人的下注额度
-  function IsBigLuckyReady() public view returns(bool) {
-      return this.balance > bigLuckyValue * bigLuckyPlayers + baseBet * minPlayers;
   }
 
   //返回总的投资次数
@@ -129,48 +118,24 @@ contract EthLottery is Owner{
         if(!IsLuckReady()) return false;
 
         uint lotteryIndex = luckyBettors.length;
-        
+
         uint winnerIndex = Random(minPlayers);
         winnerIndex = lotteryIndex * minPlayers + winnerIndex;
         address winner = bettors[winnerIndex];
-        
+
         uint luckyValue = GetLuckyValue();
         //发放奖金
         if(this.balance > luckyValue) {
             winner.transfer(luckyValue);
         }
-        
+
         luckyBettors.push(winner);
-        
+        msg.sender.transfer(luckyFee);
         return true;
     }
 
-  //检查是否可发大奖
-  function CheckBigLucky() private returns(bool) {
-    //需要满足大奖条件
-    if(!IsBigLuckyReady()) return false;
-
-    uint oldIndex = 0;
-    uint bigLuckyIndex = lotteryCountHistory.length;
-    uint lotteryIndex = luckyBettors.length;
-    //获取所有阶段全部开过奖的次数
-    for(uint i = 0; i < bigLuckyIndex; i++)
-      oldIndex += lotteryCountHistory[bigLuckyIndex];
-    //计算需要开奖的基数(开奖次数)
-    uint num = lotteryIndex - oldIndex;
-    for( i = 0; i < bigLuckyPlayers;i++){
-      uint index = Random(num * minPlayers);
-      address winner = bettors[index + oldIndex * minPlayers];
-      bigLuckyBettors.push(winner);
-      //赢者发奖金
-      winner.transfer(bigLuckyValue);
-    }
-    lotteryCountHistory.push(num);
-    return true;
-  }
-
   //查看资金池
-  function GetAwardPool() public view returns(uint){
+  function GetPool() public view returns(uint){
     return this.balance;
   }
 
